218c6c9b7ad028f88cb16ebcaec959c15070eae8
Andres Emedan <a.j.emedan@gmail.com>
2022-11-09 21:47:38 +0100

    main.js modified

--- js/main.js
+++ js/main.js
@@ -1,15 +1,14 @@
-// document.querySelector('button').addEventListener('click', apiRequest)
+document.querySelector('button').addEventListener('click', apiRequest)
 
-// async function apiRequest(){
-//     const filmName = document.querySelector('input').value
-//     try{
-//         const response = await fetch(`http://localhost:3000/api/${filmName}`)
-//         const data = await response.json()
+async function apiRequest(){
+    const filmName = document.querySelector('input').value
+    try{
+        const response = await fetch(` https://ghibli-api-fun.herokuapp.com/api/${filmName}`)
+        const data = await response.json()
 
-//         console.log(data)
-        
-//     }catch(error){
-//         console.log(error)
-//     }
-// }
-alert('Working')
+        console.log(data)
+        document.querySelector('h2').innerText = data.director   
+    }catch(error){
+        console.log(error)
+    }
+}
